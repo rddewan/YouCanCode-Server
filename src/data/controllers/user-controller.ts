cb9e58c70a8d635977a6818e7dd6373da5869420
Richard Dewan <richarddewan@hotmail.com>
2024-05-01 08:39:44 +0700

    type annotation request body

--- src/data/controllers/user-controller.ts
+++ src/data/controllers/user-controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
+import { IUserDto } from '../dtos/user.dto';
 
 export const me = async (req: Request, res: Response) => {
  
+    res.status(200).send("Hello from user controller");
+}
+
+export const create = async (req: Request<{},{}, IUserDto>, res: Response) => {
+     
     res.status(200).send("Hello from user controller");
 }
---
+++ src/data/dtos/user.dto.ts
@@ -0,0 +1,9 @@
+
+export interface IUserDto {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+    role?: string;
+    authType?: string;
+}
