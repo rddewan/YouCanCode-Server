6cdf30d66e1f69033d76d2d6f4e73cb691bcc7a0
Richard Dewan <richarddewan@hotmail.com>
2024-05-02 08:44:02 +0700

    type annotate response interface

--- src/data/controllers/user-controller.ts
+++ src/data/controllers/user-controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { IUserDto } from '../dtos/user.dto';
 import { UserQueryParams } from '../../model/types/user-query-params';
 import { UserRouteParams } from '../../model/types/user-route-params';
+import { UserReponse } from '../../model/types/user-response';
 
 
 export const me = async (req: Request, res: Response) => {
@@ -14,9 +15,14 @@ export const create = async (req: Request<{},{}, IUserDto>, res: Response) => {
     res.status(200).send("Hello from user controller");
 }
 
-export const getUserById = async (req: Request<UserRouteParams,{},{},UserQueryParams>, res: Response<{id?: String}>) => {
+export const getUserById = async (req: Request<UserRouteParams,{},{},UserQueryParams>, res: Response<UserReponse>) => {
     const routeParamsId = req.params.id;
     const userId = req.query.id;
     
-    res.status(200).json({id: routeParamsId});
+    res.status(200).json({
+        status: 'success',
+        data: {
+            id: userId || routeParamsId
+        }
+    })
 }
---
+++ src/model/types/user-response.ts
@@ -0,0 +1,6 @@
+
+
+export type UserReponse = {
+    status: string,
+    data?: {}
+}
