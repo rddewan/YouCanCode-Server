f09c870bbf0decb8d35f7c6354047e051a19f0d8
Richard Dewan <richarddewan@hotmail.com>
2024-05-14 08:47:26 +0700

    added login route

--- src/route/auth-routes.ts
+++ src/route/auth-routes.ts
@@ -1,9 +1,14 @@
 import { RequestHandler, Router } from "express";
-import { signup, verifyEmail } from "../data/controllers/auth/auth-controller";
+import {
+	login,
+	signup,
+	verifyEmail,
+} from "../data/controllers/auth/auth-controller";
 
 const router = Router();
 
 router.post("/signup", signup as RequestHandler);
+router.post("/login", login as RequestHandler);
 router.patch("/verify-email/:token", verifyEmail as RequestHandler);
 
 export default router;
